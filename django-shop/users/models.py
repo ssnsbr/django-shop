f4ea0861ffc46d1f8a2459ec42f0198f2954c7ed
sasan saberi <sasan.saberi.m@gmail.com>
2024-10-22 08:56:07 +0330

    users app

---
+++ django-shop/users/models.py
@@ -0,0 +1,28 @@
+# Create your models here.
+import uuid
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    is_vendor = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=True)
+
+
+class UserAddress(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="addresses"
+    )
+    ostan = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    address_line_1 = models.CharField(max_length=250)
+    address_line_2 = models.CharField(max_length=250)
+    phone = models.CharField(max_length=250)
+
+    # country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.country}"
---
+++ django-shop/users/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from . import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.CustomUser
+        fields = (
+            "email",
+        )
+
+
+class UserAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserAddress
+        fields = "__all__"
